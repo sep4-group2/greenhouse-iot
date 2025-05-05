3fdc4053612a5781404027f865a36d7c538689f8
DominikVIA <144497193+DominikVIA@users.noreply.github.com>
2025-05-05 16:14:25 +0200

    dominik/feat: creating a mqtt_publish method; adding a create_publish_packet helper function

--- lib/mqtt/mqtt.c
+++ lib/mqtt/mqtt.c
@@ -44,11 +44,37 @@ void mqtt_connect( char *ssid, char *password, char *ip, uint16_t port, char *cl
 
     char connect_packet_buf[200];
     int connect_packet_buflen = sizeof(connect_packet_buf);
-    int connect_packet_len = create_mqtt_connect_packet(connect_packet_buf, connect_packet_buflen);
+    int connect_packet_len = create_connect_packet(
+        connect_packet_buf, connect_packet_buflen, client_id
+    );
+    
     wifi_command_TCP_transmit(connect_packet_buf, connect_packet_len);
+
 }
 
-static int create_connect_packet(char *buf, int buflen, char *client_id)
+void mqtt_publish( char *topic, char *payload, int dup_flag, int qos_flag, int retained_flag )
+{
+    short packet_id;
+    char transmit_buf[200];
+    int transmit_buflen = sizeof(transmit_buf);
+
+    if(qos_flag > 0){
+        packet_id = 1;
+    }
+        packet_id = 0;
+
+    int transmit_len = create_publish_packet(
+        topic, payload, transmit_buf, transmit_buflen,
+        dup_flag, qos_flag, retained_flag, packet_id
+    );
+
+    wifi_command_TCP_transmit(transmit_buf, transmit_len);
+
+}
+
+// helper functions for packet creation n stuff
+
+static int create_connect_packet( char *buf, int buflen, char *client_id )
 {
     MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
     int len = 0;
@@ -60,4 +86,23 @@ static int create_connect_packet(char *buf, int buflen, char *client_id)
 
     len = MQTTSerialize_connect(buf, buflen, &data);
     return len;
+}
+
+static int create_publish_packet ( 
+    char *topic, char *payload, char *buf, int buflen,
+    int dup_flag, int qos_flag, int retained_flag, short packet_id )
+{
+
+    MQTTString topicString = MQTTString_initializer;
+    int payloadlen = strlen(payload);
+    int len = 0;
+
+    topicString.cstring = topic;
+
+    len = MQTTSerialize_publish(
+        buf, buflen, dup_flag, qos_flag, retained_flag, 
+        packet_id, topicString, payload, payloadlen
+    );
+    return len;
+
 }
--- lib/mqtt/mqtt.h
+++ lib/mqtt/mqtt.h
@@ -5,4 +5,5 @@ void mqtt_init();
 
 void mqtt_connect(char *ssid, char *password, char *ip, uint16_t port, char *client_id);
 
-void mqtt_publish(char *topic, char *payload);
+void mqtt_publish(char *topic, char *payload,
+    int dup_flag, int qos_flag, int retained_flag);
