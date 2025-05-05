2b321a17da6e97be64fbd849d41154e923909267
DominikVIA <144497193+DominikVIA@users.noreply.github.com>
2025-05-05 18:42:29 +0200

    dominik/feat: in mqtt.c better logging while establishing connections; in main.c added a basic working loop as a mqtt client

--- src/main.c
+++ src/main.c
@@ -1,57 +1,75 @@
-#include "wifi.h"
-#include "uart.h"
 #include <util/delay.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <avr/interrupt.h>
 
-static uint8_t _buff[100];
-static uint8_t _index = 0;
-volatile static bool _done = false;
-void console_rx(uint8_t _rx)
-{
-    uart_send_blocking(USART_0, _rx);
-    if(('\r' != _rx) && ('\n' != _rx))
-    {
-        if(_index < 100-1)
-        {
-            _buff[_index++] = _rx;
-        }
-    }
-    else
-    {
-        _buff[_index] = '\0';
-        _index = 0;
-        _done = true;
-        uart_send_blocking(USART_0, '\n');
-//        uart_send_string_blocking(USART_0, (char*)_buff);
-    }
+#include "mqtt.h"
+#include "periodic_task.h"
+#include "light.h"
+#include "dht11.h"
+
+// static uint8_t _buff[100];
+// static uint8_t _index = 0;
+// volatile static bool _done = false;
+// void console_rx(uint8_t _rx)
+// {
+//     uart_send_blocking(USART_0, _rx);
+//     if(('\r' != _rx) && ('\n' != _rx))
+//     {
+//         if(_index < 100-1)
+//         {
+//             _buff[_index++] = _rx;
+//         }
+//     }
+//     else
+//     {
+//         _buff[_index] = '\0';
+//         _index = 0;
+//         _done = true;
+//         uart_send_blocking(USART_0, '\n');
+// //        uart_send_string_blocking(USART_0, (char*)_buff);
+//     }
+// }
+
+void loop(){
+
+    char *topic = "greenhouse/sensors";
+
+    uint8_t humidity_int, humidity_dec, temperature_int, temperature_dec;
+
+    dht11_get(&humidity_int, &humidity_dec, &temperature_int, &temperature_dec);
+
+    uint16_t light_int = light_read();
+    char payload[80] = "";
+    sprintf(payload, "{\"air_temperature\":%d,\"air_humidity\":%d,\"soil_humidity\":%d,\"light_level\":%d}", temperature_int, humidity_int, 20, light_int);
+
+    mqtt_publish( topic, payload, 0, 0, 0 );
+
 }
 
 int main()
 {
-    char welcome_text[] = "Welcome from SEP4 IoT hardware!\n";
-    char prompt_text[] = "Type text to send: ";
 
-    uart_init(USART_0, 9600, console_rx);
-    wifi_init();
+    mqtt_init();
+    light_init();
+    dht11_init();
 
     sei();
 
-    wifi_command_join_AP("Erlands SEP4", "ViaUC1234");
-    wifi_command_create_TCP_connection("192.168.137.102", 23, NULL, NULL);
-    wifi_command_TCP_transmit((uint8_t*)welcome_text, strlen(welcome_text) );
-    uart_send_string_blocking(USART_0, prompt_text);
+    char *ssid = "ssid";
+    char *password = "password";
+    char *ip = "ip";
+    int port = 1883;
+    char *client_id = "client1";
 
-    while(1)
-    {
-        if(_done)
-        {
-            wifi_command_TCP_transmit(_buff, strlen((char*)_buff) );
-            _done = false;
-            uart_send_string_blocking(USART_0, prompt_text);
-        }
+    mqtt_connect( ssid, password, ip, port, client_id );
+
+    periodic_task_init_a(loop, 5000);
+
+    while(1){
+
     }
+
     return 0;
 }
