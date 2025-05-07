19de028a133665d78f8bd905c672412a7ee70568
DominikVIA <144497193+DominikVIA@users.noreply.github.com>
2025-05-07 13:53:58 +0200

    dominik/feat: adding subscribe functionality; reworking tcp callback to allow parsing of multiple mqtt packets from one tcp frame

--- lib/mqtt/mqtt.h
+++ lib/mqtt/mqtt.h
@@ -5,9 +5,11 @@
 
 void mqtt_init();
 
-WIFI_ERROR_MESSAGE_t mqtt_connect(char *ssid, char *password, char *ip, uint16_t port, char *client_id);
+WIFI_ERROR_MESSAGE_t mqtt_connect ( char *ssid, char *password, char *ip, uint16_t port, char *client_id );
 
-WIFI_ERROR_MESSAGE_t mqtt_reconnect(char *ip, uint16_t port, char *client_id);
+WIFI_ERROR_MESSAGE_t mqtt_reconnect ( char *ip, uint16_t port, char *client_id );
 
-WIFI_ERROR_MESSAGE_t mqtt_publish(char *topic, char *payload,
-    int dup_flag, int qos_flag, int retained_flag);
+WIFI_ERROR_MESSAGE_t mqtt_publish ( char *topic, char *payload,
+    int dup_flag, int qos_flag, int retained_flag );
+
+WIFI_ERROR_MESSAGE_t mqtt_subscribe ( char *topic, int dup_flag, int qos_flag );
