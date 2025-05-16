45dd136521042c88aefb3255c34697fd7d09a629
DominikVIA <144497193+DominikVIA@users.noreply.github.com>
2025-05-16 12:40:31 +0200

    dominik/fix: fixing issue with delays in periodic tasks; cleaning up periodic task code; cleaning up main loop; adding extra water pump logic to main loop

--- lib/drivers/periodic_task.c
+++ lib/drivers/periodic_task.c
@@ -16,13 +16,9 @@ static void (*user_func_a)(void);  // Pointer to first user function
 static void (*user_func_b)(void);  // Pointer to second user function
 static void (*user_func_c)(void);  // Pointer to third user function
 
-static uint16_t ocr3a_value = 0;
-static uint16_t ocr3b_value = 0;
+static uint16_t ocr3_value = 0;
 static uint16_t ocr3c_value = 0;
-
-static uint16_t ocr4a_value = 0;
-static uint16_t ocr4b_value = 0;
-static uint16_t ocr4c_value = 0;
+static uint16_t ocr4_value = 0;
 
 static uint16_t cnt_a = 0;
 static uint16_t cnt_b = 0;
@@ -38,7 +34,7 @@ static uint16_t loops_c = 0;
 ISR(TIMER3_COMPA_vect) {
     if (cnt_a==0)
     {
-        OCR3A=OCR3A+ocr3a_value;
+        OCR3A=OCR3A+ocr3_value;
         cnt_a=loops_a;
         user_func_a();
         
@@ -47,28 +43,17 @@ ISR(TIMER3_COMPA_vect) {
         cnt_a--;
     }
 }
-// ISR(TIMER3_COMPB_vect) {
-//     if (cnt_b==0)
-//     {
-//         OCR3B=OCR3B+ocr3b_value;
-//         cnt_b=loops_b;
-        
-//         user_func_b();
-//         periodic_task_disable_b();
-//     }
-//     else{
-//         cnt_b--;
-//     }
-// }
 
 ISR(TIMER4_COMPB_vect) {
     if (cnt_b==0)
     {
-        OCR4B=OCR4B+ocr4b_value;
+
+        OCR4B=OCR4B+ocr4_value;
         cnt_b=loops_b;
+        periodic_task_disable_b();
         
         user_func_b();
-        periodic_task_disable_b();
+
     }
     else{
         cnt_b--;
@@ -134,54 +119,35 @@ void periodic_task_init_a(void (*user_function_a)(void), uint32_t interval_ms_a)
     */
     cnt_a = (interval_ms_a/1000*(F_CPU/1024))/0xFFFF;//(interval_ms_a*125)>>19;
     loops_a=cnt_a;
-    ocr3a_value=(uint32_t)interval_ms_a*(F_CPU/1024)/1000-(uint32_t)cnt_a*0xFFFF;
+    ocr3_value=(uint32_t)interval_ms_a*(F_CPU/1024)/1000-(uint32_t)cnt_a*0xFFFF;
     
-    OCR3A = ocr3a_value;
+    OCR3A = ocr3_value;
 
     // Enable Timer3 Compare Match A interrupt
     TIMSK3 |= (1 << OCIE3A);
 }
 
-// void periodic_task_init_b(void (*user_function_b)(void), uint32_t interval_ms_b) {
-//     user_func_b = user_function_b;
-//     init_timer3();
-
-//     cnt_b = (interval_ms_b/1000*(F_CPU/1024))/0xFFFF;//(interval_ms_a*125)>>19;
-//     loops_b=cnt_b;
-//     ocr3b_value=(uint32_t)interval_ms_b*(F_CPU/1024)/1000-(uint32_t)cnt_b*0xFFFF;
-    
-//     OCR3B = (TCNT3 + ocr3b_value) % 65536;
-
-//     // Enable Timer3 Compare Match B interrupt
-//     TIMSK3 |= (1 << OCIE3B);
-// }
-
-// void periodic_task_disable_b( ) {
-
-//     user_func_b = NULL;
-//     cnt_b = 0;
-//     TIMSK3 &= ~(1 << OCIE3B);
-
-// }
 
 void periodic_task_init_b(void (*user_function_b)(void), uint32_t interval_ms_b) {
     user_func_b = user_function_b;
     init_timer4();
+    // TCNT4 = 0;
 
     cnt_b = (interval_ms_b/1000*(F_CPU/1024))/0xFFFF;//(interval_ms_a*125)>>19;
     loops_b=cnt_b;
-    ocr4b_value=(uint32_t)interval_ms_b*(F_CPU/1024)/1000-(uint32_t)cnt_b*0xFFFF;
+    ocr4_value=(uint32_t)interval_ms_b*(F_CPU/1024)/1000-(uint32_t)cnt_b*0xFFFF;
     
-    OCR4B = (TCNT3 + ocr4b_value) % 65536;
+    uint32_t next_compare = (uint32_t)TCNT4 + ocr4_value;
+    OCR4B = (uint16_t)(next_compare & 0xFFFF);
 
     // Enable Timer3 Compare Match B interrupt
+    TIFR4 |= (1 << OCF4B);
     TIMSK4 |= (1 << OCIE4B);
 }
 
 void periodic_task_disable_b( ) {
 
-    user_func_b = NULL;
-    cnt_b = 0;
+    // TIFR1 |= (1 << OCF1A);
     TIMSK4 &= ~(1 << OCIE4B);
 
 }
