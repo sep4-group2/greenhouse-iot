d9fac0efc513414de984ad3e7abd3e13163af794
DominikVIA <144497193+DominikVIA@users.noreply.github.com>
2025-05-17 13:34:13 +0200

    dominik/feat: adding logic for sending notification

---
+++ lib/notification/notification.c
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <string.h>
+
+#include "mqtt.h"
+#include "clock.h"
+
+extern char mac_address[18];
+
+void notification_send( char *action, bool status ){
+
+    char *topic = "greenhouse/action";
+    char *status_text = (status ? "true" : "false" );
+    char timestamp[21];
+    clock_to_string( &global_clock, timestamp, sizeof(timestamp) );
+
+    char payload[200];
+
+    strcat( payload, "{\"MacAddress\":\"");
+    strcat( payload, mac_address);
+    strcat( payload, "\",\"Command\":\"");
+    strcat( payload, action);
+    strcat( payload, "\",\"Status\":");
+    strcat( payload, status_text);
+    strcat( payload, "\",\"Timestamp\":\"");
+    strcat( payload, timestamp);
+    strcat( payload, "\"}");
+
+    mqtt_publish( topic, payload, 0, 0, 0 );
+
+}
---
+++ lib/notification/notification.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void notification_send( char *text );
