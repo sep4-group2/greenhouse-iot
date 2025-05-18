116aeb3027412628809901ab7c86701a6ab237b4
pattyyepez <150443257+pattyyepez@users.noreply.github.com>
2025-05-18 22:42:35 +0200

    test: clock with unity.h

---
+++ test/test_win_clock/test_win_clock.c
@@ -0,0 +1,90 @@
+#include "unity.h"
+#include "clock.h"
+#include <string.h>
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_clock_init_sets_all_fields() {
+    Clock c;
+    clock_init(&c, 2025, 5, 18, 12, 34, 56);
+    TEST_ASSERT_EQUAL_UINT16(2025, c.year);
+    TEST_ASSERT_EQUAL_UINT8(5, c.month);
+    TEST_ASSERT_EQUAL_UINT8(18, c.day);
+    TEST_ASSERT_EQUAL_UINT8(12, c.hour);
+    TEST_ASSERT_EQUAL_UINT8(34, c.minute);
+    TEST_ASSERT_EQUAL_UINT8(56, c.second);
+}
+
+void test_clock_tick_increments_seconds() {
+    Clock c = {2025, 5, 18, 12, 34, 56};
+    clock_tick(&c);
+    TEST_ASSERT_EQUAL_UINT8(57, c.second);
+}
+
+void test_clock_tick_rolls_over_seconds_to_minute() {
+    Clock c = {2025, 5, 18, 12, 34, 59};
+    clock_tick(&c);
+    TEST_ASSERT_EQUAL_UINT8(0, c.second);
+    TEST_ASSERT_EQUAL_UINT8(35, c.minute);
+}
+
+void test_clock_tick_rolls_over_minutes_to_hour() {
+    Clock c = {2025, 5, 18, 12, 59, 59};
+    clock_tick(&c);
+    TEST_ASSERT_EQUAL_UINT8(0, c.second);
+    TEST_ASSERT_EQUAL_UINT8(0, c.minute);
+    TEST_ASSERT_EQUAL_UINT8(13, c.hour);
+}
+
+void test_clock_tick_rolls_over_hours_to_day() {
+    Clock c = {2025, 5, 18, 23, 59, 59};
+    clock_tick(&c);
+    TEST_ASSERT_EQUAL_UINT8(0, c.second);
+    TEST_ASSERT_EQUAL_UINT8(0, c.minute);
+    TEST_ASSERT_EQUAL_UINT8(0, c.hour);
+    TEST_ASSERT_EQUAL_UINT8(19, c.day);
+}
+
+void test_clock_tick_rolls_over_day_to_month() {
+    Clock c = {2025, 1, 31, 23, 59, 59};
+    clock_tick(&c);
+    TEST_ASSERT_EQUAL_UINT8(1, c.day);
+    TEST_ASSERT_EQUAL_UINT8(2, c.month);
+}
+
+void test_clock_tick_rolls_over_month_to_year() {
+    Clock c = {2025, 12, 31, 23, 59, 59};
+    clock_tick(&c);
+    TEST_ASSERT_EQUAL_UINT8(1, c.day);
+    TEST_ASSERT_EQUAL_UINT8(1, c.month);
+    TEST_ASSERT_EQUAL_UINT16(2026, c.year);
+}
+
+void test_clock_to_string_formats_correctly() {
+    Clock c = {2025, 5, 18, 9, 4, 7};
+    char buf[32];
+    clock_to_string(&c, buf, sizeof(buf));
+    TEST_ASSERT_EQUAL_STRING("2025-05-18T09:04:07Z", buf);
+}
+
+void test_clock_update_task_advances_global_clock() {
+    global_clock = (Clock){2025, 5, 18, 10, 0, 0};
+    clock_update_task();
+    TEST_ASSERT_EQUAL_UINT8(1, global_clock.second);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_clock_init_sets_all_fields);
+    RUN_TEST(test_clock_tick_increments_seconds);
+    RUN_TEST(test_clock_tick_rolls_over_seconds_to_minute);
+    RUN_TEST(test_clock_tick_rolls_over_minutes_to_hour);
+    RUN_TEST(test_clock_tick_rolls_over_hours_to_day);
+    RUN_TEST(test_clock_tick_rolls_over_day_to_month);
+    RUN_TEST(test_clock_tick_rolls_over_month_to_year);
+    RUN_TEST(test_clock_tick_leap_year);
+    RUN_TEST(test_clock_to_string_formats_correctly);
+    RUN_TEST(test_clock_update_task_advances_global_clock);
+    return UNITY_END();
+}
