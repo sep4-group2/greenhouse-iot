1bd96e26f3704b340686a47c39c04fe0a16f0447
DominikVIA <144497193+DominikVIA@users.noreply.github.com>
2025-05-20 10:55:47 +0200

    dominik/feat: replacing received_publish with received_packet

--- lib/mqtt_buffer/mqtt_buffer.h
+++ lib/mqtt_buffer/mqtt_buffer.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #define BUFFER_CAPACITY 25
 
 typedef struct mqtt_buffer* mqtt_buffer_t;
---
+++ lib/mqtt_received_publish/mqtt_received_packet.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "mqtt_received_packet.h"
+
+typedef struct mqtt_received_packet {
+    unsigned char packet_type; 
+    char* buf;
+    int len;
+} mqtt_received_packet;
+
+mqtt_received_packet_t mqtt_received_packet_init( unsigned char packet_type, char* buf, int len ){
+
+    mqtt_received_packet_t _received_packet = calloc( 1, sizeof(mqtt_received_packet) );
+
+    if( NULL == _received_packet ) return NULL;
+
+    if( NULL == buf ) return NULL;
+
+    memcpy( _received_packet->buf, buf, len );
+
+    _received_packet->packet_type = packet_type;
+    _received_packet-> len = len;
+
+    return _received_packet;
+
+}
+
+char *mqtt_received_packet_get_buf( mqtt_received_packet_t self ){
+    return self->buf;
+}
+
+int mqtt_received_packet_get_len( mqtt_received_packet_t self ){
+    return self->len;
+}
+
+char mqtt_received_packet_get_type( mqtt_received_packet_t self ){
+    return self->packet_type;
+}
+
+void mqtt_received_packet_free( mqtt_received_packet_t self ){
+    if(NULL!= self) free(self);
+}
