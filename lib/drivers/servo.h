39a9649430eb18414a566dfefb696281ad504619
pattyyepez <150443257+pattyyepez@users.noreply.github.com>
2025-05-20 22:58:51 +0200

    maria/test: servo

--- lib/drivers/servo.h
+++ lib/drivers/servo.h
@@ -1,2 +1,13 @@
+#ifndef SERVO_H
+#define SERVO_H
+
 #include <stdint.h>
+#include <avr/io.h>
+
+#define DDR_SERVO DDRE
+#define PORT_SERVO PORTE
+#define P_SERVO PE3
+
 void servo(uint8_t angle);
+
+#endif 
